7425ab64d854b94926caca8c641c6b409842bf69
Yuliya Mironova <rabkevich15@gmail.com>
2022-07-18 12:41:32 +0200

    feat: parcel-bundler

--- ts/feachers.ts
+++ ts/feachers.ts
@@ -1,3 +1,25 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+axios.get(url).then((response) => {
+	const todo = response.data as Todo;
+	const id = todo.id;
+	const title = todo.title;
+	const completed = todo.completed;
+	logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+	console.log(`it is ${id} with ${title} and ${completed}`);
+};
+
 const today: Date = new Date();
 //console.log(today.getMonth() + 1);
 const appels: number = 5;
