2bdb03a5d30815cfc38e35c7da0989e6aff6d68f
Yuliya Mironova <rabkevich15@gmail.com>
2022-07-18 22:39:29 +0200

    feat: map

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { User } from "./User";
+/*import { User } from "./User";
 import { Company } from "./Company";
 const user = new User();
 console.log(user);
 
 const company = new Company();
-console.log(company);
+console.log(company);*/
+
+new google.maps.Map(document.getElementById("map"));
