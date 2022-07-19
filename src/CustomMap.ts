dae7cb96f55e9522436ec0f736d6f7dbcb394faa
Yuliya Mironova <rabkevich15@gmail.com>
2022-07-19 12:31:38 +0200

    feat:interface mappable

---
+++ src/CustomMap.ts
@@ -0,0 +1,46 @@
+/*import { User } from "./User";
+import { Company } from "./Company";*/
+
+interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+}
+
+export class CustomMap {
+	private googleMap: google.maps.Map;
+
+	constructor(divID: string) {
+		this.googleMap = new google.maps.Map(
+			document.getElementById(divID) as HTMLElement,
+			{
+				zoom: 1,
+				center: {
+					lat: 0,
+					lng: 0,
+				},
+			}
+		);
+	}
+
+	addMarker(mappable: Mappable): void {
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: mappable.location.lat,
+				lng: mappable.location.lng,
+			},
+		});
+	}
+
+	/*addCompanyMarker(company: Company): void {
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: company.location.lat,
+				lng: company.location.lng,
+			},
+		});
+	}*/
+}
